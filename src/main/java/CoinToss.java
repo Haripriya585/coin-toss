bd92ba0d995dee1a7e5517711479108cce43fcf1
Your Name <you@example.com>
2021-04-05 23:09:34 +0530

    Created method to compare probability of head and tail in coin toss

---
+++ src/main/java/CoinToss.java
@@ -0,0 +1,11 @@
+public class CoinToss {
+    public boolean toCompareProbabilityOfHeadIsEqualToProbabilityOfTail() {
+        double probabilityOfHead=0.5;
+        double probabilityOfTail=0.5;
+        if(probabilityOfHead==probabilityOfTail){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
